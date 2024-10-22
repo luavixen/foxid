448d53371ba7e2e68a68dec686275b41577e9c48
Lua MacDougall <lua@foxgirl.dev>
2024-10-22 02:42:49 +0000

    Fix TypeScript not finding types for browsers

---
+++ lib/foxid-browser.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Generates a secure and URL-friendly unique ID.
+ * @param {number} [length] - Length of the generated ID, defaults to 16.
+ * @returns {string} Newly generated ID as a string.
+ */
+declare function foxid(length?: number): string
+export = foxid
