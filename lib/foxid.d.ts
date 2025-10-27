a17193e710337415b904c7b6355208471ad3fa59
Lua MacDougall <lua@foxgirl.dev>
2025-10-27 13:15:36 -0700

    Refactor into single file

--- lib/foxid.d.ts
+++ lib/foxid.d.ts
@@ -1,7 +1,13 @@
 /**
- * Generates a secure and URL-friendly unique ID.
- * @param {number} [length] - Length of the generated ID, defaults to 16.
- * @returns {string} Newly generated ID as a string.
+ * Generates a secure URL-friendly unique ID.
+ *
+ * @param {number} [length] Length of the generated ID, defaults to 16.
+ * @returns {string} Generated ID as a string.
+ *
+ * @example
+ * var foxid = require('foxid')
+ * foxid() // "w9q567s7qj4bmp9h"
+ * foxid(32) // "tw24cymw3kb225mctevc3csna241pnm2"
  */
 declare function foxid(length?: number): string
 export = foxid
